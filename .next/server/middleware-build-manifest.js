d3a3999bb679def51283987b1afab9a593da7e30
Numan Usman <92387051+Nuestman@users.noreply.github.com>
2025-05-25 08:17:24 +0000

    Add .next/server/middleware-build-manifest.js 1748161041682

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,47 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/": [
+      "static/chunks/node_modules_next_63387682._.js",
+      "static/chunks/node_modules_react-dom_82bb97c6._.js",
+      "static/chunks/node_modules_8764e743._.js",
+      "static/chunks/[root-of-the-server]__23b907f9._.js",
+      "static/chunks/src_pages_index_5771e187._.js",
+      "static/chunks/src_pages_index_9f9e3f3e._.js"
+    ],
+    "/_app": [
+      "static/chunks/node_modules_next_dist_4f3d469a._.js",
+      "static/chunks/node_modules_react-dom_82bb97c6._.js",
+      "static/chunks/node_modules_8764e743._.js",
+      "static/chunks/[root-of-the-server]__659f318b._.js",
+      "static/chunks/src_pages__app_5771e187._.js",
+      "static/chunks/src_pages__app_9a4c604f._.js"
+    ],
+    "/auth/register": [
+      "static/chunks/node_modules_next_63387682._.js",
+      "static/chunks/node_modules_react-dom_82bb97c6._.js",
+      "static/chunks/node_modules_2a5cf4bb._.js",
+      "static/chunks/[root-of-the-server]__d3091bfe._.js",
+      "static/chunks/src_pages_auth_register_tsx_5771e187._.js",
+      "static/chunks/src_pages_auth_register_tsx_87bdbfed._.js"
+    ],
+    "/auth/signin": [
+      "static/chunks/node_modules_next_dist_4f3d469a._.js",
+      "static/chunks/node_modules_react-dom_82bb97c6._.js",
+      "static/chunks/node_modules_8764e743._.js",
+      "static/chunks/[root-of-the-server]__a7ab6857._.js",
+      "static/chunks/src_pages_auth_signin_tsx_5771e187._.js",
+      "static/chunks/src_pages_auth_signin_tsx_d3a103c8._.js"
+    ]
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
